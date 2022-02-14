8c5ea214296b45677309799bad9c805ace5b9c0b
YogaTgar123 <yogatgar4@gmail.com>
2022-02-13 19:28:30 -0800

    first commit

---
+++ praktikum2_rpl4/resources/views/kontak.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('judul_halaman')
+
+@section ('konten')
+
+    <h2>Ini Adalah Halaman Kontak</h2>
+    <p>Nama : Yoga Tegar Pratama</p>
+    <p>No Telp : 087731365437</p>
+    <p>Instagram : @yogatgar_</p>
+
+
+@endsection
